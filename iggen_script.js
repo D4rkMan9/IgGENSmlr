998d250c57a8779b4545e93a507d5ac273be8833
D4rkMan9 <119982369+D4rkMan9@users.noreply.github.com>
2025-02-13 03:13:15 -0300

    Add files via upload

---
+++ iggen_script.js
@@ -0,0 +1,29 @@
+function capturar() {
+    const elemento = document.getElementById("captura_preview");
+
+    html2canvas(elemento, {
+        scale: 4 // Ajusta la escala para aumentar la resolución (3x para obtener 1080x1920)
+    }).then(canvas => {
+        let link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "captura.png";
+        link.click();
+    });
+}
+
+
+
+function generar_cap (){
+    let string = document.getElementById('texto').value.trim();
+    let value = document.getElementById("texto_size").value;
+
+    document.getElementById('content_preview').innerHTML = string || 'No ingresaste texto';
+
+    document.getElementById("content_preview").style.fontSize = value + "px";  // Usamos 'value' directamente
+
+    
+}
+
+
+
+
